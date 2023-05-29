9d45649c98d04fb60b6ea10210bcdc4f1bbb5cce
celiandre <celia.andre.2012@gmail.com>
2023-05-29 10:09:12 +0200

    first commit

---
+++ send-email.php
@@ -0,0 +1,38 @@
+<?php
+
+$name=$_POST["name"];
+$email=$_POST["email"];
+$subject=$_POST["subject"];
+$message=$_POST["message"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+
+$mail = new PHPMailer(true);
+
+
+$mail->isSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPAuth = true;
+$mail->SMTPDebug=1;
+$mail->SMTPSecure="tls";
+
+$mail->Host="smtp.gmail.com";
+$mail->Port=587;
+$mail->Username = "harmoniedesete@gmail.com";
+$mail->Password = "@jmsml13A2012!";
+
+$mail->setFrom($email,$name);
+$mail->addAddress("celiagrosjean@hotmail.com","Test");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+echo'email send';
